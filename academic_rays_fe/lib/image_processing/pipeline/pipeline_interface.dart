9e0bbba15a2ba89415fa25b2a0f8f222aef76704
Duck Ran <puranlai@qq.com>
2026-02-04 21:56:52 +0800

    refactor: implement image processing job base class

--- academic_rays_fe/lib/image_processing/pipeline/pipeline_interface.dart
+++ academic_rays_fe/lib/image_processing/pipeline/pipeline_interface.dart
@@ -1,5 +1,5 @@
 import 'dart:typed_data';
-import 'dart:ui';
+import 'package:meta/meta.dart';
 
 /// 任务状态
 enum JobStatus {
@@ -9,16 +9,38 @@ enum JobStatus {
   error,
 }
 
-/// 图片处理任务抽象接口
+/// 图片处理任务抽象基类
 /// [R] 任务返回的结果类型
 abstract class ImageProcessingJob<R> {
-  /// 当前任务状态
-  JobStatus get status;
+  JobStatus _status = JobStatus.idle;
+  R? _lastResult;
 
-  /// 执行任务
-  /// [imageData] 输入图片的二进制数据
-  Future<R> execute(Uint8List imageData);
+  /// 当前任务状态
+  JobStatus get status => _status;
 
   /// 获取最近一次处理的结果
-  R? get lastResult;
+  R? get lastResult => _lastResult;
+
+  /// 执行任务的统一入口
+  /// [imageData] 输入图片的二进制数据
+  Future<R> execute(Uint8List imageData) async {
+    if (_status == JobStatus.processing) {
+      throw Exception("Job is already running");
+    }
+
+    _status = JobStatus.processing;
+    try {
+      final result = await process(imageData);
+      _lastResult = result;
+      _status = JobStatus.completed;
+      return result;
+    } catch (e) {
+      _status = JobStatus.error;
+      rethrow;
+    }
+  }
+
+  /// 子类应实现此方法来执行具体逻辑
+  @protected
+  Future<R> process(Uint8List imageData);
 }
